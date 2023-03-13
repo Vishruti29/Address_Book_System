b873a6351beb4f5597c939c31b877c2b8c2aed14
Vishruti Patil <vishruti2908@gmail.com>
2023-03-13 14:16:23 +0530

    new contacts added successfully

--- src/com/Day9/AddressBook.java
+++ src/com/Day9/AddressBook.java
@@ -1,17 +1,16 @@
 package com.Day9;
 import java.util.Scanner;
-public class AddressBookSystem {
+public class AddressBook {
     public static void main(String[] args) {
-        System.out.println(" Welcome to Address Book ");
-        System.out.println(" Enter Number 1 (add) , 0 (Exit) ");
-        Scanner sc = new Scanner(System.in);
-        int input = sc.nextInt();
-
-        int exit = 0;
-        do {
-            switch (input) {
+        System.out.println(" Address Book ");
+        System.out.println(" Enter Number 1(add) , 0(Exit) ");
+        Scanner sc =new Scanner(System.in);
+        int input =sc.nextInt();
+        int exit=0;
+        do{
+            switch (input){
                 case 0:
-                    exit = 1;
+                    exit=1;
                     System.out.println("  Left Thank You ...");
                     break;
                 case 1:
@@ -19,18 +18,17 @@ public class AddressBookSystem {
                     break;
             }
 
-        } while (exit != 1);
+        }while (exit!=1);
+
     }
-
-
-    static void add() {
-        Scanner sc = new Scanner(System.in);
+    static void add(){
+        Scanner sc=new Scanner(System.in);
 
         System.out.println("Enter First Name :");
-        String name = sc.nextLine();
+        String firstName = sc.nextLine();
 
         System.out.println("Enter Last Name :");
-        String last_n = sc.nextLine();
+        String lastName = sc.nextLine();
 
         System.out.println("Enter City :");
         String city = sc.nextLine();
@@ -41,16 +39,17 @@ public class AddressBookSystem {
         System.out.println("Enter Zip :");
         String zip = sc.nextLine();
 
-        System.out.println("Phone Number :");
-        String number = sc.nextLine();
+        System.out.println("Enter Number :");
+        String phoneNumber = sc.nextLine();
 
-        System.out.println("Enter email :");
+        System.out.println("Enter Email :");
         String email = sc.nextLine();
 
         System.out.println("Contact Added Successfully...");
 
     }
-    static class Contact {
+
+    class Contact {
         private String firstName;
         private String lastName;
         private String city;
@@ -68,5 +67,6 @@ public class AddressBookSystem {
             this.phoneNumber = phoneNumber;
             this.email = email;
         }
+
     }
 }
